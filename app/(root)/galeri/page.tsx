fdaa16ce5990125c7dfc15eeeb96296dac683f9e
n00b St4r <realgoldcraft@gmail.com>
2024-01-06 22:55:04 +0700

    change data order galeri

--- app/(root)/galeri/page.tsx
+++ app/(root)/galeri/page.tsx
@@ -4,7 +4,11 @@ import Client from "./components/client";
 export const revalidate = 0;
 
 export default async function Galeri() {
-  const data = await prismadb.tb_galeri.findMany();
+  const data = await prismadb.tb_galeri.findMany({
+    orderBy: {
+      created_at: "desc",
+    },
+  });
 
   return (
     <>
