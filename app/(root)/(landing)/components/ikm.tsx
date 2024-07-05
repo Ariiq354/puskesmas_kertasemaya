47644782fcf2d86ba91155a6f016a1ad06f138bd
n00b St4r <realgoldcraft@gmail.com>
2024-07-05 12:43:49 +0700

    add ikm

--- app/(root)/(landing)/components/ikm.tsx
+++ app/(root)/(landing)/components/ikm.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Ikm() {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <div className="flex flex-wrap flex-row items-center align-middle justify-center max-w-5xl h-full py-20 gap-14 md:gap-20 px-10">
+      <div className="flex container flex-wrap flex-row items-center align-middle justify-center  h-full py-20 gap-14 md:gap-20 px-10">
         <Image
           className="flex flex-initial rounded-lg object-cover shadow-2xl border-black border-solid bord h-auto"
           src="/ikm_1.jpg"
@@ -15,6 +15,34 @@ export default function Ikm() {
         <Image
           className="flex flex-initial rounded-lg object-cover shadow-2xl h-auto"
           src="/ikm_2.jpg"
+          width={300}
+          height={300}
+          alt=""
+        />
+        <Image
+          className="flex flex-initial rounded-lg object-cover shadow-2xl h-auto"
+          src="/ikm_3.jpeg"
+          width={300}
+          height={300}
+          alt=""
+        />
+        <Image
+          className="flex flex-initial rounded-lg object-cover shadow-2xl h-auto"
+          src="/ikm_4.jpeg"
+          width={300}
+          height={300}
+          alt=""
+        />
+        <Image
+          className="flex flex-initial rounded-lg object-cover shadow-2xl h-auto"
+          src="/ikm_5.jpeg"
+          width={300}
+          height={300}
+          alt=""
+        />
+        <Image
+          className="flex flex-initial rounded-lg object-cover shadow-2xl h-auto"
+          src="/ikm_6.jpeg"
           width={300}
           height={300}
           alt=""
