1f5f4f57007e61730e1d71ac54bde04d881f5886
Ariiq354 <realgoldcraft@gmail.com>
2025-05-13 09:36:19 +0700

    add ikm 9

--- app/(root)/(landing)/components/ikm.tsx
+++ app/(root)/(landing)/components/ikm.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 import React from "react";
 
 export default function Ikm() {
-  const items = Array.from({ length: 8 }, (_, index) => index);
-  
+  const items = Array.from({ length: 9 }, (_, index) => index);
+
   return (
     <div className="w-full h-full flex items-center justify-center">
       <div className="flex container flex-wrap flex-row items-center align-middle justify-center  h-full py-20 gap-14 md:gap-20 px-10">
